33548b1d17fb5f5acfebb63ee7175d1e71613b43
Yoo-uijin <timetam24@naver.com>
2023-03-20 01:13:12 +0900

    Modify data

--- simplediary/src/DiaryItem.js
+++ simplediary/src/DiaryItem.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
-const DiaryItem = ({ date, content, emotion, id, onRemove }) => {
+const DiaryItem = ({ date, content, emotion, id, onRemove, onEdit }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const toggleIsEdit = () => setIsEdit(!isEdit);
+
+  const [localContent, setLocalContent] = useState(content);
+  const localContentInput = useRef();
+
   const handleRemove = () => {
     if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
       onRemove(id);
+    }
+  };
+
+  const handleQuitEdit = () => {
+    setIsEdit(false);
+    setLocalContent(content);
+  };
+
+  const handleEdit = () => {
+    if (localContent.length < 1) {
+      localContentInput.current.focus();
+      return;
+    }
+
+    if (window.confirm(`${id}번째 일기를 수정하시겠습니까?`)) {
+      onEdit(id, localContent);
+      toggleIsEdit();
     }
   };
 
@@ -14,8 +37,30 @@ const DiaryItem = ({ date, content, emotion, id, onRemove }) => {
           날짜 : {date} | 감정점수 : {emotion}
         </span>
       </div>
-      <div className="content">{content}</div>
-      <button onClick={handleRemove}>삭제</button>
+      <div className="content">
+        {isEdit ? (
+          <>
+            <textarea
+              ref={localContentInput}
+              value={localContent}
+              onChange={(e) => setLocalContent(e.target.value)}
+            />
+          </>
+        ) : (
+          <>{content}</>
+        )}
+      </div>
+      {isEdit ? (
+        <>
+          <button onClick={handleQuitEdit}>수정 취소</button>
+          <button onClick={handleEdit}>수정 완료</button>
+        </>
+      ) : (
+        <>
+          <button onClick={handleRemove}>삭제</button>
+          <button onClick={toggleIsEdit}>수정</button>
+        </>
+      )}
     </li>
   );
 };
