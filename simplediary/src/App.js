31ea1520067c194358ba47e8006dd788b00ae23a
Yoo-uijin <timetam24@naver.com>
2023-03-21 03:02:05 +0900

    Controlling the React Lifecycle

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -3,6 +3,7 @@ import { useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
+import Lifecycle from "./Lifecycle";
 
 // const dummyList = [
 //   {
@@ -56,6 +57,7 @@ function App() {
 
   return (
     <div className="App">
+      <Lifecycle />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onRemove={onRemove} diaryList={data} onEdit={onEdit} />
     </div>
