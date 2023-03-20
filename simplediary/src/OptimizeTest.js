46251ee31be141eed749d7ccf715368890724688
Yoo-uijin <timetam24@naver.com>
2023-03-21 06:10:22 +0900

    Update OptimizeTest.js

--- simplediary/src/OptimizeTest.js
+++ simplediary/src/OptimizeTest.js
@@ -1,43 +1,7 @@
-import React, { useEffect, useState } from "react";
-
-const Textview = React.memo(({ text }) => {
-  useEffect(() => {
-    console.log({ text });
-  });
-  return <div>{text}</div>;
-});
-
-const Countview = React.memo(({ count }) => {
-  useEffect(() => {
-    console.log({ count });
-  });
-  return <div>{count}</div>;
-});
+import React from "react";
 
 const OptimizeTest = () => {
-  const [count, setCount] = useState(1);
-  const [text, setText] = useState("");
-
-  return (
-    <div style={{ padding: 50 }}>
-      <div>
-        <h2>count</h2>
-        <Countview count={count} />
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          +
-        </button>
-      </div>
-      <div>
-        <h2>text</h2>
-        <Textview text={text} />
-        <input value={text} onChange={(e) => setText(e.target.value)} />
-      </div>
-    </div>
-  );
+  return <div style={{ padding: 50 }}></div>;
 };
 
 export default OptimizeTest;
